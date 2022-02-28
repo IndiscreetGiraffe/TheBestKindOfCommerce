bca990299b541def5da4c9f2b6fa04b1492875ef
Kyleah Justice <kikij432@gmail.com>
2022-02-28 01:51:12 -0700

    checked over the routes underneath and filled out the delete a product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -89,7 +89,9 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name, price: req.body.price, stock: req.body.stock, category_id: req.body.category_id, tagIds: req.body.tagIds
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -155,6 +157,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(productData => {
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id"});
+      return;
+    }
+    res.json({ message: "Successfully deleted the product!"})
+  })
 });
 
 module.exports = router;
