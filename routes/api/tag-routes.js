c36b968ba114e119a248343b547c11c785375228
Kyleah Justice <kikij432@gmail.com>
2022-02-28 01:58:24 -0700

    finished the get by single id tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,8 +31,30 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    // be sure to include its associated Product data
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+  })
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: "Not tag with this id"});
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    res.status(500).json({ message: "An error has been made"})
+  });
 });
 
 router.post('/', (req, res) => {
